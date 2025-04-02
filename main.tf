d952b4464bdb5019d6089061ef7185a44205837b
CliffordCheung <cliffordcheung77@gmail.com>
2025-04-02 20:59:56 +0800

    Adding new s3 bucket 11th time

--- main.tf
+++ main.tf
@@ -24,6 +24,26 @@ locals {
   account_id  = data.aws_caller_identity.current.account_id
 }
 
-resource "aws_s3_bucket" "s3_tf" {
+# resource "aws_s3_bucket" "s3_tf" {
+#   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+# }
+
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "mybucket" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
